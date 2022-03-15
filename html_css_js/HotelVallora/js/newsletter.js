d840363274d8d212213965cf9ec3d8348fbd3934
Tim <timhofmann01@gmail.com>
2022-03-15 16:09:27 +0100

    more wip

--- html_css_js/HotelVallora/js/newsletter.js
+++ html_css_js/HotelVallora/js/newsletter.js
@@ -2,21 +2,27 @@ window.onsubmit = function () {
     let firstname = document.getElementById('vorname').value;
     let lastname = document.getElementById('nachname').value;
     let mail = document.getElementById('mail').value;
-    let log = document.createElement("p");
+    let log = document.createElement('p');
 
-    let err = validateName(firstname, lastname);
+    let err = [validateName(firstname, lastname), validateMail(firstname, lastname)];
 
-    if (err) {
-        log.appendChild(err);
-        document.getElementsByTagName("body")[0].appendChild(log);
+    if (err.length > 0) {
+        let errnode = document.createTextNode(err);
+        log.appendChild(errnode);
+
+        document.getElementById("logs").appendChild(log);
         alert(err);
     }
 
-    if (!isValidMail(mail)) {
-        log.appendChild("Sie haben keine gültige E-Mail-Adresse angegeben.");
-        document.getElementById("log")[0].appendChild(log);
-        alert("Sie haben keine gültige E-Mail-Adresse angegeben.");
-    }
+    // if (!isValidMail(mail)) {
+    //     let errnode = document.createTextNode("Sie haben keine gültige E-Mail-Adresse angegeben.");
+    //     log.appendChild(errnode);
+
+    //     document.getElementById("logs").appendChild(log);
+    //     alert("Sie haben keine gültige E-Mail-Adresse angegeben.");
+    // }
+
+    return false;
 }
 
 function validateName(firstname, lastname) {
@@ -30,10 +36,10 @@ function validateName(firstname, lastname) {
     return null;
 }
 
-function isValidMail(mail) {
+function validateMail(mail) {
     if (!mail.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-        return false;
+        return "Sie haben keine gültige E-Mail-Adresse angegeben.";
     }
 
-    return true;
+    return null;
 }
