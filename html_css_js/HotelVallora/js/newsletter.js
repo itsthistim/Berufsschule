5d784189124649eb6a07cf88519d6a8745eb04ae
Tim <timhofmann01@gmail.com>
2022-03-16 09:20:52 +0100

    finished core tasks

--- html_css_js/HotelVallora/js/newsletter.js
+++ html_css_js/HotelVallora/js/newsletter.js
@@ -1,23 +1,34 @@
-window.onsubmit = function () {
+const form = document.getElementById('form');
+const dropdown1 = document.getElementById('aufmerksamvon');
+const log = document.getElementById('logs');
+
+form.addEventListener('submit', validateSubmit);
+
+function validateSubmit(event) {
     let firstname = document.getElementById('vorname').value;
     let lastname = document.getElementById('nachname').value;
     let mail = document.getElementById('mail').value;
+    let err = [validateName(firstname, lastname), validateMail(mail), validateAufmerksamkeit(dropdown1.value)];
 
-    let err = [validateName(firstname, lastname), validateMail(mail)];
+    let hasErr = false;
 
-    if (err.length > 0) {
+    for (let i = 0; i < err.length; i++) {
+        const error = err[i];
+        if (error != undefined) {
+            hasErr = true;
+            break;
+        }
+    }
+
+    if (hasErr) {
         err.forEach(error => {
             if (error) {
-                let log = document.createElement('p');
-                let errnode = document.createTextNode(error);
-                log.appendChild(errnode);
-                document.getElementById("logs").appendChild(log);
+                log.innerHTML += `${error}<br>`;
                 alert(error);
             }
         });
+        event.preventDefault();
     }
-
-    return false;
 }
 
 function validateName(firstname, lastname) {
@@ -27,14 +38,16 @@ function validateName(firstname, lastname) {
     if (firstname == lastname) {
         return "Vor- und Nachname dürfen nicht gleich sein."
     }
+}
 
-    return null;
+function validateAufmerksamkeit(value) {
+    if (!value) {
+        return "Wovon sind Sie auf uns Aufmerksam geworden? Bitte füllen das Feld aus."
+    }
 }
 
 function validateMail(mail) {
     if (!mail.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
         return "Sie haben keine gültige E-Mail-Adresse angegeben.";
     }
-
-    return null;
 }
