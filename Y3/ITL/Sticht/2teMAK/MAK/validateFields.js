85af17ead4d1704746c694d80de99ffa70fa979f
Tim <timhofmann01@gmail.com>
2022-03-23 14:05:07 +0100

    fixed faulty form

--- Y3/ITL/Sticht/2teMAK/MAK/validateFields.js
+++ Y3/ITL/Sticht/2teMAK/MAK/validateFields.js
@@ -1,9 +1,14 @@
+window.onload = function() {
+    document.getElementById('contactform').onsubmit = checkFields;
+}
+
 function checkFields() {
     let firstname = document.getElementById('firstname');
     let lastname = document.getElementById('lastname');
     let email = document.getElementById('email');
     let country = document.getElementById('country');
     let plz = document.getElementById('PLZ');
+    let isValid = true;
 
     let welcomer = document.getElementById('welcomer');
 
@@ -12,35 +17,43 @@ function checkFields() {
     log.style.color = "red";
 
     let err = [];
-
+    
     //required fields
     if (!firstname.value) {
-        err.push("You did not enter a first name.")
+        err.push("You did not enter a first name.");
+        isValid = false;
     }
     if (!lastname.value) {
-        err.push("You did not enter a last name.")
+        err.push("You did not enter a last name.");
+        isValid = false;
     }
     if (!email.value) {
-        err.push("You did not enter an e-mail.")
+        err.push("You did not enter an e-mail.");
+        isValid = false;
     }
     if (!country.value) {
-        err.push("You did not enter a country.")
+        err.push("You did not enter a country.");
+        isValid = false;
     }
     if (!plz.value) {
-        err.push("You did not enter a country code.")
+        err.push("You did not enter a country code.");
+        isValid = false;
     }
 
     // no duplicate names
     if (firstname.value == lastname.value) {
         err.push("Your first name and your last name can't be the same.")
+        isValid = false;
     }
 
     if (!checkLen(firstname.value, 2)) {
         err.push("Your first name is too short.");
+        isValid = false;
     }
 
     if (!checkLen(lastname.value, 3)) {
         err.push("Your last name is too short.");
+        isValid = false;
     }
 
     
@@ -50,6 +63,7 @@ function checkFields() {
 
     // change text in hero
     welcomer.innerText = `Welcome, ${firstname.value} ${lastname.value}`;
+    return isValid;
 }
 
 function checkLen(str, len) {
