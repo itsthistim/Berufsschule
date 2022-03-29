62b1f70c724f14d31ce49f428da6537fe7bea612
Tim <timhofmann01@gmail.com>
2022-03-29 23:00:49 +0200

    started outsourcing blog elements

--- Y3/ITL/Sticht/bootstrap/koala/index.php
+++ Y3/ITL/Sticht/bootstrap/koala/index.php
@@ -15,44 +15,41 @@
     <link href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/twitter/53/koala_1f428.png"
       rel="apple-touch-icon">
 
-    <?php include "./views/elements/body_begin.php" ?>
+    <?php include "./views/elements/index/body_begin.php" ?>
 
   </head>
 
   <body>
 
     <!-- ======= Header ======= -->
-    <?php include "./views/elements/header.php" ?>
+    <?php include "./views/elements/index/header.php" ?>
 
     <!-- ======= Hero Section ======= -->
-    <?php include "./views/elements/hero.php" ?>
+    <?php include "./views/elements/index/hero.php" ?>
 
     <main id="main">
 
       <!-- ======= About Section ======= -->
-      <?php include "./views/elements/about.php" ?>
+      <?php include "./views/elements/index/about.php" ?>
 
       <!-- ======= Blog ======= -->
-      <?php include "./views/elements/blog_portfolio.php" ?>
-      <?php include "./views/elements/blog_features.php" ?>
+      <?php include "./views/elements/index/blog_portfolio.php" ?>
+      <?php include "./views/elements/index/blog_features.php" ?>
   
       <!-- ======= Cta Section ======= -->
-      <?php //include "./views/elements/cta.php" ?>
+      <?php //include "./views/elements/index/cta.php" ?>
 
     </main>
     <!-- End #main -->
 
     <!-- ======= Footer ======= -->
-    <?php include './views/elements/footer.php'; ?>
+    <?php include './views/elements/index/footer.php'; ?>
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
         class="bi bi-arrow-up-short"></i></a>
 
-    <?php include './views/elements/body_end.php'; ?>
+    <?php include './views/elements/index/body_end.php'; ?>
 
   </body>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
 
 </html>
