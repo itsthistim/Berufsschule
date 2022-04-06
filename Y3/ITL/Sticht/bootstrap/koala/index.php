dbe4f59530c668248e974f5224f67e8fb3b8977e
Tim <timhofmann01@gmail.com>
2022-04-06 21:04:07 +0200

    commented out expiry

--- Y3/ITL/Sticht/bootstrap/koala/index.php
+++ Y3/ITL/Sticht/bootstrap/koala/index.php
@@ -16,61 +16,39 @@ $SESSION['user_id'] = user_id;
 // $newArticle = new Article(Utils::nextId("articles"), active_project, 1, "TEST TITLE", "blog", "This is a test article", "This is the body of the test article", "message-intents.jpg", 1, date("Y-m-d H:i:s"), date("Y-m-d H:i:s"));
 // $newArticle->insert();
 
-// Utils::resetDB();
+//Utils::resetDB();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
   <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
     <title>koala</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
-    <!-- Favicons -->
-    <link href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/twitter/53/koala_1f428.png"
-      rel="icon">
-    <link href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/twitter/53/koala_1f428.png"
-      rel="apple-touch-icon">
-
-    <!-- ======= Body Begin ======= -->
     <?php include "./views/elements/index/body_begin.php" ?>
-
   </head>
 
   <body>
-
-    <!-- ======= Header ======= -->
     <?php include "./views/elements/index/header.php" ?>
 
-    <!-- ======= Hero Section ======= -->
     <?php include "./views/elements/index/hero.php" ?>
-
-    <!-- ======= Main ======= -->
+    
     <main id="main">
-      <?php // phpinfo(); ?>
-
-      <!-- ======= About Section ======= -->
       <?php include "./views/elements/index/about.php" ?>
 
-      <!-- ======= Blog ======= -->
       <?php include "./views/elements/index/blog_listing.php" ?>
       <?php //include "./views/elements/index/blog_features.php" ?>
       <?php include "./views/elements/index/topFive.php" ?>
 
-      <!-- ======= Cta Section ======= -->
       <?php // include "./views/elements/index/cta.php" ?>
     </main>
 
-    <!-- ======= Footer ======= -->
     <?php include './views/elements/index/footer.php'; ?>
     
-    <!-- ======= Body End ======= -->
     <?php include './views/elements/index/body_end.php'; ?>
 
   </body>
-
 </html>
