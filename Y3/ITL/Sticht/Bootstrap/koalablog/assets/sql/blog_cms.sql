dd131bb7626b0817a9fbdeb5dde5b6df100df714
Tim <timhofmann01@gmail.com>
2022-04-21 09:27:58 +0200

    rewrote resetDB

---
+++ Y3/ITL/Sticht/Bootstrap/koalablog/assets/sql/blog_cms.sql
@@ -0,0 +1,306 @@
+-- -----------------------------------------------------
+-- Table `blog_cms`.`users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog_cms`.`users`;
+CREATE TABLE IF NOT EXISTS `blog_cms`.`users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(45) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `created` DATETIME NULL DEFAULT NULL,
+    `modified` DATETIME NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
+    UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+ALTER TABLE `blog_cms`.`users` AUTO_INCREMENT = 1;
+
+-- -----------------------------------------------------
+-- Table `blog_cms`.`projects`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog_cms`.`projects`;
+CREATE TABLE IF NOT EXISTS `blog_cms`.`projects` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `image` VARCHAR(255) NULL DEFAULT NULL,
+    `color_main` CHAR(8) NULL DEFAULT NULL,
+    `color_secondary` CHAR(8) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+ALTER TABLE `blog_cms`.`projects` AUTO_INCREMENT = 1;
+
+-- -----------------------------------------------------
+-- Table `blog_cms`.`articles`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog_cms`.`articles`;
+CREATE TABLE IF NOT EXISTS `blog_cms`.`articles` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `project_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `slug` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NULL DEFAULT NULL,
+    `body` TEXT NULL DEFAULT NULL,
+    `image` VARCHAR(255) NULL DEFAULT NULL,
+    `published` TINYINT(1) NULL DEFAULT '0',
+    `created` DATETIME NULL DEFAULT NULL,
+    `modified` DATETIME NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `slug` (`slug` ASC) VISIBLE,
+    INDEX `user_key` (`user_id` ASC) VISIBLE,
+    INDEX `project_key` (`project_id` ASC) VISIBLE,
+    CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `blog_cms`.`users` (`id`),
+    CONSTRAINT `articles_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `blog_cms`.`projects` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 8 DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+ALTER TABLE `blog_cms`.`articles` AUTO_INCREMENT = 1;
+
+-- -----------------------------------------------------
+-- Table `blog_cms`.`tags`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog_cms`.`tags`;
+CREATE TABLE IF NOT EXISTS `blog_cms`.`tags` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NULL DEFAULT NULL,
+    `created` DATETIME NULL DEFAULT NULL,
+    `modified` DATETIME NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `title` (`title` ASC) VISIBLE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+ALTER TABLE `blog_cms`.`tags` AUTO_INCREMENT = 1;
+
+-- -----------------------------------------------------
+-- Table `blog_cms`.`articles_tags`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog_cms`.`articles_tags`;
+CREATE TABLE IF NOT EXISTS `blog_cms`.`articles_tags` (
+    `article_id` INT NOT NULL,
+    `tag_id` INT NOT NULL,
+    PRIMARY KEY (`article_id`, `tag_id`),
+    INDEX `tag_key` (`tag_id` ASC) VISIBLE,
+    CONSTRAINT `articles_tags_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `blog_cms`.`tags` (`id`),
+    CONSTRAINT `articles_tags_ibfk_2` FOREIGN KEY (`article_id`) REFERENCES `blog_cms`.`articles` (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+ALTER TABLE `blog_cms`.`articles_tags` AUTO_INCREMENT = 1;
+
+-- -----------------------------------------------------
+-- Inserts
+-- -----------------------------------------------------
+
+
+INSERT INTO
+    users (username, email, password, created, modified)
+VALUES
+    ("Tim Hofmann", "timhofmann01@gmail.com", "12345", NOW(), NOW());
+
+INSERT INTO
+    projects (title, description)
+VALUES
+    ("Discord Bot", "koala discord bot");
+
+INSERT INTO
+    articles(
+        project_id,
+        user_id,
+        title,
+        slug,
+        description,
+        body,
+        image,
+        published,
+        created,
+        modified
+    )
+VALUES
+    (
+        1,
+        1,
+        "Image Manipulation",
+        "image-manipulation",
+        "How to make custom images with koala",
+        "Creating funny images has never been this easy. With koala you can now change your friends profile pictures to your liking!",
+        "image-manipulation.jpg",
+        1,
+        NOW(),
+        NOW()
+    );
+
+INSERT INTO
+    articles(
+        project_id,
+        user_id,
+        title,
+        slug,
+        description,
+        body,
+        image,
+        published,
+        created,
+        modified
+    )
+VALUES
+(
+        1,
+        1,
+        "Internationalization & Languages",
+        "internationalization-languages",
+        "Use koala with nearly any language",
+        "We're including most spoken languages from around the globe including English, Chinese, French, Spanish, Polish, Russian and more! You can even help us translating if your language is not available yet!",
+        "internationalization.jpg",
+        1,
+        NOW(),
+        NOW()
+    );
+
+INSERT INTO
+    articles(
+        project_id,
+        user_id,
+        title,
+        slug,
+        description,
+        body,
+        image,
+        published,
+        created,
+        modified
+    )
+VALUES
+(
+        1,
+        1,
+        "Message Intents",
+        "message-intents",
+        "What are Message Intents?",
+        "If your bot or app is not approved for message content, the following fields of the message object in Gateway and API payloads will be empty—either an empty string or empty array, depending on the data type—when you receive a message: content, embeds, attachments, components.",
+        "message-intents.jpg",
+        1,
+        NOW(),
+        NOW()
+    );
+
+INSERT INTO
+    articles(
+        project_id,
+        user_id,
+        title,
+        slug,
+        description,
+        body,
+        image,
+        published,
+        created,
+        modified
+    )
+VALUES
+(
+        1,
+        1,
+        "Music System",
+        "music-system",
+        "Playing music to your friends the easy way",
+        "Play high quality music in your Discord server for free. If it' s on YouTube, then koala can find it!",
+        "music-system.jpg",
+        1,
+        NOW(),
+        NOW()
+    );
+
+INSERT INTO
+    articles(
+        project_id,
+        user_id,
+        title,
+        slug,
+        description,
+        body,
+        image,
+        published,
+        created,
+        modified
+    )
+VALUES
+(
+        1,
+        1,
+        "Logging & Moderation",
+        "logging-moderation",
+        "How logging and moderation makes a better community",
+        "Discord moderation bots help organize your server, act as a welcome bot for new members, and work as a Discord anti spam bot for troublemakers. koala fills all these roles and if you are working as a Discord moderator, then you try it for yourself ! ",
+        "logging-moderation.jpg",
+        1,
+        NOW(),
+        NOW()
+    );
+
+INSERT INTO
+    articles(
+        project_id,
+        user_id,
+        title,
+        slug,
+        description,
+        body,
+        image,
+        published,
+        created,
+        modified
+    )
+VALUES
+(
+        1,
+        1,
+        "Slash Commands",
+        "slash-commands",
+        "Why / will soon be your most used key",
+        "Slash Commands are an extremely powerful way to provide rich interactivity for members of your Discord server, all you have to do is type / and you 're ready to use your favorite bot. You can easily see all the bot' s commands, input validation, and error handling help you get the command right the first time.",
+        "slash-commands.jpg",
+        1,
+        NOW(),
+        NOW()
+    );
+
+INSERT INTO
+    tags (title, created, modified)
+VALUES
+    ("API", NOW(), NOW());
+
+INSERT INTO
+    tags (title, created, modified)
+VALUES
+    ("Categories", NOW(), NOW());
+
+INSERT INTO
+    tags (title, created, modified)
+VALUES
+    ("Misc", NOW(), NOW());
+
+INSERT INTO
+    articles_tags (article_id, tag_id)
+VALUES
+    (1, 2);
+
+INSERT INTO
+    articles_tags (article_id, tag_id)
+VALUES
+    (2, 3);
+
+INSERT INTO
+    articles_tags (article_id, tag_id)
+VALUES
+    (3, 1);
+
+INSERT INTO
+    articles_tags (article_id, tag_id)
+VALUES
+    (4, 2);
+
+INSERT INTO
+    articles_tags (article_id, tag_id)
+VALUES
+    (5, 2);
+
+INSERT INTO
+    articles_tags (article_id, tag_id)
+VALUES
+    (6, 1);
--- Y3/ITL/Sticht/Bootstrap/koalablog/models/utils.php
+++ Y3/ITL/Sticht/Bootstrap/koalablog/models/utils.php
@@ -43,149 +43,19 @@ class Utils extends DB
         $stmt->execute([$table, $int]);
     }
 
-    public static function resetDB()
+    public static function resetDb()
     {
         $db = new DB();
-
-        #region drop tables, recreate tables, reset increment
-        //$stmt = $db->pdo->prepare("SET GLOBAL information_schema_stats_expiry = 0;");
-        //$stmt->execute();
-        
-        // drop tables
-        $stmt = $db->pdo->prepare("SET FOREIGN_KEY_CHECKS = 0;");
+        // set foreign key checks to 0
+        $stmt = $db->pdo->prepare("SET FOREIGN_KEY_CHECKS = 0");
         $stmt->execute();
 
-        $stmt = $db->pdo->prepare("DROP TABLE `articles`;");
+        // creates & inserts
+        $stmt = $db->pdo->prepare(file_get_contents("assets/sql/blog_cms.sql"));
         $stmt->execute();
 
-        $stmt = $db->pdo->prepare("DROP TABLE `users`;");
+        // set foreign key checks to 1
+        $stmt = $db->pdo->prepare("SET FOREIGN_KEY_CHECKS = 1");
         $stmt->execute();
-
-        $stmt = $db->pdo->prepare("DROP TABLE `tags`;");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("DROP TABLE `articles_tags`;");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("DROP TABLE `projects`;");
-        $stmt->execute();
-
-        // create tables
-        $stmt = $db->pdo->prepare("CREATE TABLE IF NOT EXISTS `users` ( 
-            id INT AUTO_INCREMENT PRIMARY KEY, 
-            email VARCHAR(255) NOT NULL, 
-            password VARCHAR(255) NOT NULL, 
-            created DATETIME, 
-            modified DATETIME 
-        );");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("CREATE TABLE IF NOT EXISTS `projects` ( 
-            id INT AUTO_INCREMENT PRIMARY KEY, 
-            title VARCHAR(255) NOT NULL, 
-            description TEXT NOT NULL,
-            image VARCHAR(255),
-            color_main char(8), 
-            color_secondary char(8)   
-        );");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("CREATE TABLE IF NOT EXISTS `articles` ( 
-            id INT AUTO_INCREMENT PRIMARY KEY, 
-            project_id INT NOT NULL,  
-            user_id INT NOT NULL, 
-            title VARCHAR(255) NOT NULL, 
-            slug VARCHAR(255) NOT NULL, 
-            description VARCHAR(255),
-            body TEXT,
-            image VARCHAR(255),
-            published BOOLEAN DEFAULT FALSE, 
-            created DATETIME, 
-            modified DATETIME, 
-            UNIQUE KEY (slug), 
-            FOREIGN KEY user_key (user_id) REFERENCES users(id), 
-            FOREIGN KEY project_key (project_id) REFERENCES projects(id) 
-        );");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("CREATE TABLE IF NOT EXISTS `tags` ( 
-            id INT AUTO_INCREMENT PRIMARY KEY, 
-            title VARCHAR(191), 
-            created DATETIME, 
-            modified DATETIME, 
-            UNIQUE KEY (title) 
-        ) CHARSET=utf8mb4;");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("CREATE TABLE IF NOT EXISTS `articles_tags` ( 
-            article_id INT NOT NULL, 
-            tag_id INT NOT NULL, 
-            PRIMARY KEY (article_id, tag_id), 
-            FOREIGN KEY tag_key(tag_id) REFERENCES tags(id), 
-            FOREIGN KEY article_key(article_id) REFERENCES articles(id) 
-        );");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("SET FOREIGN_KEY_CHECKS = 1;");
-        $stmt->execute();
-        #endregion
-
-        #region insert data
-        // users
-        $stmt = $db->pdo->prepare("INSERT INTO users (email, password, created, modified) VALUES (\"tim\", \"1234\", NOW(), NOW());");
-        $stmt->execute();
-
-        // projects
-        $stmt = $db->pdo->prepare("INSERT INTO projects (title, description) VALUES (\"Discord Bot\", \"koala discord bot\");");
-        $stmt->execute();
-
-        // articles
-        $stmt = $db->pdo->prepare("INSERT INTO articles(project_id, user_id, title, slug, description, body, image, published, created, modified) VALUES(1, 1, \"Image Manipulation\", \"image-manipulation\", \"How to make custom images with koala\", \"Creating funny images has never been this easy. With koala you can now change your friends profile pictures to your liking!\", \"image-manipulation.jpg\", 1, NOW(), NOW());");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO articles(project_id, user_id, title, slug, description, body, image, published, created, modified) VALUES(1, 1, \"Internationalization & Languages\", \"internationalization-languages\", \"Use koala with nearly any language\", \"We're including most spoken languages from around the globe including English, Chinese, French, Spanish, Polish, Russian and more! You can even help us translating if your language is not available yet!\", \"internationalization.jpg\", 1, NOW(), NOW());");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO articles(project_id, user_id, title, slug, description, body, image, published, created, modified) VALUES(1, 1, \"Message Intents\", \"message-intents\", \"What are Message Intents?\", \"If your bot or app is not approved for message content, the following fields of the message object in Gateway and API payloads will be empty—either an empty string or empty array, depending on the data type—when you receive a message: content, embeds, attachments, components.\", \"message-intents.jpg\", 1, NOW(), NOW());");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO articles(project_id, user_id, title, slug, description, body, image, published, created, modified) VALUES(1, 1, \"Music System\", \"music-system\", \"Playing music to your friends the easy way\", \"Play high quality music in your Discord server for free. If it's on YouTube, then koala can find it!\", \"music-system.jpg\", 1, NOW(), NOW());");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO articles(project_id, user_id, title, slug, description, body, image, published, created, modified) VALUES(1, 1, \"Logging & Moderation\", \"logging-moderation\", \"How logging and moderation makes a better community\", \"Discord moderation bots help organize your server, act as a welcome bot for new members, and work as a Discord anti spam bot for troublemakers. koala fills all these roles and if you are working as a Discord moderator, then you try it for yourself!\", \"logging-moderation.jpg\", 1, NOW(), NOW());");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO articles(project_id, user_id, title, slug, description, body, image, published, created, modified) VALUES(1, 1, \"Slash Commands\", \"slash-commands\", \"Why / will soon be your most used key\", \"Slash Commands are an extremely powerful way to provide rich interactivity for members of your Discord server, all you have to do is type / and you're ready to use your favorite bot. You can easily see all the bot's commands, input validation, and error handling help you get the command right the first time.\", \"slash-commands.jpg\", 1, NOW(), NOW());");
-        $stmt->execute();
-
-        // tags
-        $stmt = $db->pdo->prepare("INSERT INTO tags (title, created, modified) VALUES (\"API\", NOW(), NOW());");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO tags (title, created, modified) VALUES (\"Categories\", NOW(), NOW());");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO tags (title, created, modified) VALUES (\"Misc\", NOW(), NOW());");
-        $stmt->execute();
-
-        // articles_tags
-        $stmt = $db->pdo->prepare("INSERT INTO articles_tags (article_id, tag_id) VALUES (1, 2);");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO articles_tags (article_id, tag_id) VALUES (2, 3);");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO articles_tags (article_id, tag_id) VALUES (3, 1);");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO articles_tags (article_id, tag_id) VALUES (4, 2);");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO articles_tags (article_id, tag_id) VALUES (5, 2);");
-        $stmt->execute();
-
-        $stmt = $db->pdo->prepare("INSERT INTO articles_tags (article_id, tag_id) VALUES (6, 1);");
-        $stmt->execute();
-    #endregion
     }
 }
