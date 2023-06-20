afdeae59142b9a124b3590866d6341e56df5d3f7
Tim <timhofmann01@gmail.com>
2023-06-20 09:37:48 +0200

    done

--- Y4/ITL/Arztpraxis/src/models/patient.js
+++ Y4/ITL/Arztpraxis/src/models/patient.js
@@ -1,3 +1,4 @@
+import { doesNotMatch } from 'assert';
 import { db } from './db';
 
 export default class Patient {
@@ -42,87 +43,19 @@ export default class Patient {
 	/**
 	 * Gets Behandlungsdaten of a patient within a time range.
 	 */
-	static async getBehandlungsdaten(svnr, birthday, start, end) {
+	static async getBehandlungsdaten(svnr, birthday, start, end, timerange = false) {
 		let behandlungsdaten = [];
 
-		if (!start && !end) {
+		if (timerange) {
 			const [rows] = await db.query(
 				`SELECT concat(b.beginn, ' - ', IF(b.ende IS NULL, 'laufend', b.ende)) as "Zeitraum", concat(p.vorname, ' ', p.nachname) as "Patient", concat(p.svnr, '/', p.geburtsdatum) as "SVNr", d.name as "Diagnose"
-            FROM person p, diagnose d, behandlungszeitraum b
-           WHERE b.person_id = p.id
-             AND b.diagnose_id = d.id
-             AND p.svnr = ?
-             AND p.geburtsdatum = ?
-        ORDER BY beginn desc;`,
-				[svnr, birthday]
-			);
-			if (rows.length === 0) throw new Error('Es wurden keine Behandlungsdaten gefunden.');
-
-			rows.forEach((row) => {
-				behandlungsdaten.push({
-					zeitraum: row.Zeitraum,
-					patient: row.Patient,
-					svnr: row.SVNr,
-					diagnose: row.Diagnose
-				});
-			});
-		} else if (start && !end) {
-			const [rows] = await db.query(
-				`SELECT concat(b.beginn, ' - ', IF(b.ende IS NULL, 'laufend', b.ende)) as "Zeitraum", concat(p.vorname, ' ', p.nachname) as "Patient", concat(p.svnr, '/', p.geburtsdatum) as "SVNr", d.name as "Diagnose"
-            FROM person p, diagnose d, behandlungszeitraum b
-              WHERE b.person_id = p.id
-                AND b.diagnose_id = d.id
-                AND p.svnr = ?
-                AND p.geburtsdatum = ?
-                AND b.beginn >= ?
-            ORDER BY beginn desc;`,
-				[svnr, birthday, start]
-			);
-
-			if (rows.length === 0) throw new Error('Es wurden keine Behandlungsdaten gefunden.');
-
-			rows.forEach((row) => {
-				behandlungsdaten.push({
-					zeitraum: row.Zeitraum,
-					patient: row.Patient,
-					svnr: row.SVNr,
-					diagnose: row.Diagnose
-				});
-			});
-		} else if (!start && end) {
-			const [rows] = await db.query(
-				`SELECT concat(b.beginn, ' - ', IF(b.ende IS NULL, 'laufend', b.ende)) as "Zeitraum", concat(p.vorname, ' ', p.nachname) as "Patient", concat(p.svnr, '/', p.geburtsdatum) as "SVNr", d.name as "Diagnose"
-            FROM person p, diagnose d, behandlungszeitraum b
-                WHERE b.person_id = p.id
-                AND b.diagnose_id = d.id
-                AND p.svnr = ?
-                AND p.geburtsdatum = ?
-                AND b.ende <= ?
-            ORDER BY beginn desc;`,
-				[svnr, birthday, end]
-			);
-
-			if (rows.length === 0) throw new Error('Es wurden keine Behandlungsdaten gefunden.');
-
-			rows.forEach((row) => {
-				behandlungsdaten.push({
-					zeitraum: row.Zeitraum,
-					patient: row.Patient,
-					svnr: row.SVNr,
-					diagnose: row.Diagnose
-				});
-			});
-		} else if (start && end) {
-			const [rows] = await db.query(
-				`SELECT concat(b.beginn, ' - ', IF(b.ende IS NULL, 'laufend', b.ende)) as "Zeitraum", concat(p.vorname, ' ', p.nachname) as "Patient", concat(p.svnr, '/', p.geburtsdatum) as "SVNr", d.name as "Diagnose"
-            FROM person p, diagnose d, behandlungszeitraum b
-                WHERE b.person_id = p.id
-                AND b.diagnose_id = d.id
-                AND p.svnr = ?
-                AND p.geburtsdatum = ?
-                AND b.beginn >= ?
-                AND b.ende <= ?
-            ORDER BY beginn desc;`,
+			FROM person p, diagnose d, behandlungszeitraum b
+			WHERE b.person_id = p.id
+				AND b.diagnose_id = d.id
+				AND p.svnr = ?
+				AND p.geburtsdatum = ?
+				AND date(b.beginn) between ? and ?
+			ORDER BY beginn desc;`,
 				[svnr, birthday, start, end]
 			);
 
@@ -137,9 +70,102 @@ export default class Patient {
 				});
 			});
 		} else {
-			throw new Error('Es wurden keine Behandlungsdaten gefunden.');
+			if (!start && !end) {
+				const [rows] = await db.query(
+					`SELECT concat(b.beginn, ' - ', IF(b.ende IS NULL, 'laufend', b.ende)) as "Zeitraum", concat(p.vorname, ' ', p.nachname) as "Patient", concat(p.svnr, '/', p.geburtsdatum) as "SVNr", d.name as "Diagnose"
+				FROM person p, diagnose d, behandlungszeitraum b
+			   WHERE b.person_id = p.id
+				 AND b.diagnose_id = d.id
+				 AND p.svnr = ?
+				 AND p.geburtsdatum = ?
+			ORDER BY beginn desc;`,
+					[svnr, birthday]
+				);
+				if (rows.length === 0) throw new Error('Es wurden keine Behandlungsdaten gefunden.');
+
+				rows.forEach((row) => {
+					behandlungsdaten.push({
+						zeitraum: row.Zeitraum,
+						patient: row.Patient,
+						svnr: row.SVNr,
+						diagnose: row.Diagnose
+					});
+				});
+			} else if (start && !end) {
+				const [rows] = await db.query(
+					`SELECT concat(b.beginn, ' - ', IF(b.ende IS NULL, 'laufend', b.ende)) as "Zeitraum", concat(p.vorname, ' ', p.nachname) as "Patient", concat(p.svnr, '/', p.geburtsdatum) as "SVNr", d.name as "Diagnose"
+				FROM person p, diagnose d, behandlungszeitraum b
+				  WHERE b.person_id = p.id
+					AND b.diagnose_id = d.id
+					AND p.svnr = ?
+					AND p.geburtsdatum = ?
+					AND date(b.beginn) >= ?
+				ORDER BY beginn desc;`,
+					[svnr, birthday, start]
+				);
+
+				if (rows.length === 0) throw new Error('Es wurden keine Behandlungsdaten gefunden.');
+
+				rows.forEach((row) => {
+					behandlungsdaten.push({
+						zeitraum: row.Zeitraum,
+						patient: row.Patient,
+						svnr: row.SVNr,
+						diagnose: row.Diagnose
+					});
+				});
+			} else if (!start && end) {
+				const [rows] = await db.query(
+					`SELECT concat(b.beginn, ' - ', IF(b.ende IS NULL, 'laufend', b.ende)) as "Zeitraum", concat(p.vorname, ' ', p.nachname) as "Patient", concat(p.svnr, '/', p.geburtsdatum) as "SVNr", d.name as "Diagnose"
+				FROM person p, diagnose d, behandlungszeitraum b
+					WHERE b.person_id = p.id
+					AND b.diagnose_id = d.id
+					AND p.svnr = ?
+					AND p.geburtsdatum = ?
+					AND b.ende <= ?
+				ORDER BY beginn desc;`,
+					[svnr, birthday, end]
+				);
+
+				if (rows.length === 0) throw new Error('Es wurden keine Behandlungsdaten gefunden.');
+
+				rows.forEach((row) => {
+					behandlungsdaten.push({
+						zeitraum: row.Zeitraum,
+						patient: row.Patient,
+						svnr: row.SVNr,
+						diagnose: row.Diagnose
+					});
+				});
+			} else if (start && end) {
+				const [rows] = await db.query(
+					`SELECT concat(b.beginn, ' - ', IF(b.ende IS NULL, 'laufend', b.ende)) as "Zeitraum", concat(p.vorname, ' ', p.nachname) as "Patient", concat(p.svnr, '/', p.geburtsdatum) as "SVNr", d.name as "Diagnose"
+				FROM person p, diagnose d, behandlungszeitraum b
+					WHERE b.person_id = p.id
+					AND b.diagnose_id = d.id
+					AND p.svnr = ?
+					AND p.geburtsdatum = ?
+					AND b.beginn >= ?
+					AND b.ende <= ?
+				ORDER BY beginn desc;`,
+					[svnr, birthday, start, end]
+				);
+
+				if (rows.length === 0) throw new Error('Es wurden keine Behandlungsdaten gefunden.');
+
+				rows.forEach((row) => {
+					behandlungsdaten.push({
+						zeitraum: row.Zeitraum,
+						patient: row.Patient,
+						svnr: row.SVNr,
+						diagnose: row.Diagnose
+					});
+				});
+			} else {
+				throw new Error('Es wurden keine Behandlungsdaten gefunden.');
+			}
 		}
 
-        return behandlungsdaten;
+		return behandlungsdaten;
 	}
 }
